289e39193d565fa399f96ba175f23fd80a5b7369
mmmz21 <emmawilcox121@gmail.com>
2020-10-11 17:03:51 -0700

    Exercise 3.2

--- index.js
+++ index.js
@@ -15,12 +15,19 @@ let persons = [
   }
 ]
 app.get('/', (req, res) => {
-  response.send('<h1> HOnebook!</h1>')
+  response.send('<h1> Phonebook!</h1>')
 })
 
 app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
+
+app.get('/info', (req, res) => {
+  const num_ppl = persons.length
+  let date = new Date()
+  res.json(`Phonebook has info for ${num_ppl} people ${date}`)
+})
+
 const PORT = 3001
 app.listen(PORT)
-console.log('Server running on port ${PORT}')
+console.log(`Server running on port ${PORT}`)
