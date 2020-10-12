882eaa3f50cc9844ee5554cf6d42941182a49a1d
mmmz21 <emmawilcox121@gmail.com>
2020-10-11 17:08:39 -0700

    Exercise 3.3 Implemented functionality for displaying a single phonebook entry
    http://localhost:3001/api/persons/5

--- index.js
+++ index.js
@@ -28,6 +28,16 @@ app.get('/info', (req, res) => {
   res.json(`Phonebook has info for ${num_ppl} people ${date}`)
 })
 
+// fetch an individual entry using it's id
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
