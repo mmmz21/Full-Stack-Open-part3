82d37c7d67ffdb7774fba246a7ee998f9d6d8cee
mmmz21 <emmawilcox121@gmail.com>
2020-10-11 17:52:35 -0700

    Exercise 3.5 New phonebook entires can be added

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const { notEqual } = require('assert')
-const { response } = require('express')
 const express = require('express')
 const app = express()
-
+//activate's the express json-parser
+app.use(express.json())
 let persons = [
   {
-    "name": "Harry",
-    "number": "Hole",
+    "name": "Harry Hole",
+    "number": "123457789",
     "id": 14
   },
   {
@@ -47,6 +46,29 @@ app.delete('/api/persons/:id', (req, res) => {
   // filter out the entry that is being deleted
   persons = persons.filter(person => person.id !== id)
   res.status(204).end()
+})
+
+// add new phonebook entries to the server 
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing'
+    })
+  }
+  else if (!body.num) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+  const person = {
+    name: body.name,
+    number: body.num,
+    id: Math.floor(Math.random() * Math.floor(100)),
+  }
+
+  persons = persons.concat(person)
+  res.json(person)
 })
 const PORT = 3001
 app.listen(PORT)
